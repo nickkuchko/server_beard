1e5e6d4dbff1c65dd70684f5a8ea7cc74d85d234
Nick Kuchko <nick.kuchko@gmail.com>
2022-03-27 20:16:05 +0300

    Update settings.py

--- project_beard/settings.py
+++ project_beard/settings.py
@@ -132,5 +132,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
